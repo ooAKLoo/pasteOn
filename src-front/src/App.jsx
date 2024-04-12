517ad11e3b260d466de0b14e1aa3ca8f5a43a3a6
Dongju <2797208347@qq.com>
2024-04-12 16:49:02 +1000

    init commit

---
+++ src-front/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div data-tauri-drag-region className="flex items-center justify-center h-screen bg-red-400 rounded-3xl">
+      <h1 className="text-4xl font-bold">Hello, Tauri!</h1>
+    </div>
+  );
+}
+
+export default App;
