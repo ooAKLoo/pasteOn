30cc62f484476b0f9a7fa7b3846245962e44e399
Dongju <2797208347@qq.com>
2024-04-13 14:00:51 +1000

    实现点击后可以拓展高度

--- src-front/src/App.jsx
+++ src-front/src/App.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import ItemsManager from './models/itemsManager';
 import { useKeyboardShortcuts } from './hook/useKeyboardShortcuts';
 import { useClipboard } from './hook/useClipboard';
-import { appWindow } from '@tauri-apps/api/window';
+import { appWindow, LogicalSize  } from '@tauri-apps/api/window';
 
 function App() {
   const [isVisible, setIsVisible] = useState(true);
   const { writeToClipboard, readFromClipboard } = useClipboard();
   const MAX_LENGTH = 5;  // Define the maximum length of the items array
+  const [windowSize, setWindowSize] = useState(new LogicalSize(400, 100));
 
   const { items, adjustIndex } = ItemsManager({ isVisible, setIsVisible, writeToClipboard, readFromClipboard, MAX_LENGTH });
 
@@ -18,9 +19,24 @@ function App() {
   }, [isVisible]);
 
   useKeyboardShortcuts(isVisible, setIsVisible, adjustIndex, () => writeToClipboard(items[index].toString()));
+  
+  const toggleWindowSize = () => {
+    const expanded = windowSize.height === 100; // Check if the window is in minimized state
+    const newHeight = expanded ? 300 : 100;  // Expand if minimized, minimize if expanded
+    const newSize = new LogicalSize(windowSize.width, newHeight);
+    appWindow.setSize(newSize)
+      .then(() => {
+        setWindowSize(newSize);
+      })
+      .catch(error => console.error('Failed to set window size:', error));
+  };
 
   return (
-    <div data-tauri-drag-region className={`flex flex-col min-h-screen p-2 text-ellipsis overflow-hidden  items-center justify-center ${isVisible ? 'bg-red-400' : 'opacity-0 pointer-events-none'} transition-opacity ease-in-out duration-500 rounded-xl`}>
+    <div
+      onDoubleClick={toggleWindowSize}
+      data-tauri-drag-region
+      className={`flex flex-col min-h-screen p-2 text-ellipsis overflow-hidden  items-center justify-center ${isVisible ? 'bg-red-400' : 'opacity-0 pointer-events-none'} transition-opacity ease-in-out duration-500 rounded-xl`}
+    >
       <h1 className=" text-base font-bold line-clamp-3">{items[0]}</h1>
     </div>
   );
