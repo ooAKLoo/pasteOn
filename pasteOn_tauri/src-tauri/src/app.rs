513db7e3cab087484282f934055efc105e33719b
Dongju <2797208347@qq.com>
2024-04-17 11:55:27 +1000

    备份tray设定
    接下来对代码进行优化

--- pasteOn_tauri/src-tauri/src/app.rs
+++ pasteOn_tauri/src-tauri/src/app.rs
@@ -1,19 +1,4 @@
-use tauri::{ AppHandle, Window };
-use tauri::Manager; // 添加这行
-use local_ip_address::local_ip;
-use crate::state::Clients;
-use std::sync::{Arc, Mutex};
-
-// pub fn start_tauri_app<F: FnOnce(AppHandle) + Send + 'static>(setup_fn: F) {
-//     tauri::Builder::default()
-//         .setup(move |app| {
-//             setup_fn(app.app_handle());
-//             Ok(())
-//         })
-//         .invoke_handler(tauri::generate_handler![send_server_details])
-//         .run(tauri::generate_context!())
-//         .expect("error while running tauri application");
-// }
+use tauri::{ Window };
 
 #[tauri::command]
 pub fn send_server_details(window: Window, ip: String, port: u16) {
--- pasteOn_tauri/src-tauri/src/main.rs
+++ pasteOn_tauri/src-tauri/src/main.rs
@@ -17,13 +17,12 @@ use crate::app::{ send_server_details };
 use std::collections::HashMap;
 use local_ip_address::local_ip;
 use once_cell::sync::Lazy;
+use tauri::{ CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, WindowBuilder };
 
 // 定义全局的服务器状态标记
 static SERVER_RUNNING: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
 
-static GLOBAL_CLIENTS: Lazy<Clients> = Lazy::new(|| {
-    Arc::new(Mutex::new(HashMap::new()))
-});
+static GLOBAL_CLIENTS: Lazy<Clients> = Lazy::new(|| { Arc::new(Mutex::new(HashMap::new())) });
 
 #[tokio::main]
 async fn main() {
@@ -31,8 +30,38 @@ async fn main() {
 
     let my_local_ip = local_ip().unwrap();
 
+    // 创建系统托盘菜单
+    let tray_menu = SystemTrayMenu::new()
+        // .add_item(CustomMenuItem::new("show", "Show"))
+        // .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_item(CustomMenuItem::new("quit", "Quit"));
+
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder
         ::default()
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| {
+            match event {
+                SystemTrayEvent::MenuItemClick { id, .. } => {
+                    let window = app.get_window("main").unwrap();
+                    match id.as_str() {
+                        // "show" => {
+                        //     window.show().unwrap();
+                        //     window.set_focus().unwrap();
+                        // }
+                        // "hide" => {
+                        //     window.hide().unwrap();
+                        // }
+                        "quit" => {
+                            std::process::exit(0);
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        })
         .setup(move |app| {
             let app_handle = app.app_handle(); // Get AppHandle
             tokio::spawn(async move {
@@ -60,7 +89,7 @@ async fn main() {
                     if !*server_running {
                         println!("Starting WebSocket server at ");
                         // 这里放置启动服务器的代码
-                        *server_running = true;  // 更新服务器运行状态为 true
+                        *server_running = true; // 更新服务器运行状态为 true
                     } else {
                         println!("Server already running at ");
                     }
@@ -71,14 +100,12 @@ async fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![start_server_if_needed ,send_server_details])
+        .invoke_handler(tauri::generate_handler![start_server_if_needed, send_server_details])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 // Ensure the websocket::start_websocket_server is updated to use ws crate
-
-
 
 #[tauri::command]
 async fn start_server_if_needed(app_handle: tauri::AppHandle) {
@@ -87,9 +114,9 @@ async fn start_server_if_needed(app_handle: tauri::AppHandle) {
     let mut server_running = SERVER_RUNNING.lock().await;
 
     if !*server_running {
-        *server_running = true;  // 更新服务器运行状态为 true
+        *server_running = true; // 更新服务器运行状态为 true
         std::thread::spawn(move || {
-            start_websocket_server(&server_address,GLOBAL_CLIENTS.clone());
+            start_websocket_server(&server_address, GLOBAL_CLIENTS.clone());
         });
         println!("WebSocket server started successfully.");
         register_mdns_service();
